32234de5fb0a5498fe74d4675d30a8d0a91967fc
CyberHurricane <151047607+CyberHurricane@users.noreply.github.com>
2024-06-25 17:31:35 +0100

    Major code rewrite and addtional functions

--- Program.cs
+++ Program.cs
@@ -1,188 +1,112 @@
 ﻿using System;
-using System.Data;
+using System.Collections.Generic;
 using System.Net.Sockets;
-using System.Reflection;
-using System.Reflection.Emit;
 using System.Threading;
 
 namespace TCPPortScanner
 {
     internal class Program
     {
+        static List<int> openPorts = new List<int>();  // List to store open ports
+        static List<int> closedPorts = new List<int>(); // List to store closed ports
+
         static void Main(string[] args)
         {
-            //Thread mainThread = Thread.CurrentThread;
-            string IP = "";
-            Stager(IP);
-            PortRange(IP);
-            Console.WriteLine("I'm Main!");
-            Console.ReadLine();
-            //Console.WriteLine(searchedports);
-        }
-        static string Stager(string IP)
-        {
-            Console.WriteLine("Enter IP");
+            string IP;
+            Console.WriteLine("Enter IP:");
             IP = Console.ReadLine();
-            return IP;
-            //PortRange(IP);
+            PortRange(IP);
+            Console.ReadLine();
         }
         static void PortRange(string IP)
         {
-            string StartPort;
-            string EndPort;
+            Console.WriteLine("Enter Start Port:");
+            int startPort = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Start Port");
-            StartPort = Console.ReadLine();
-            if (StartPort == "0")
+            if (startPort <= 0)
             {
-                Console.WriteLine("Error, Enter a number greater than 1");
-                Stager(IP);
-            }
-            int SPort = (int)Convert.ToInt64(StartPort);
-            Console.WriteLine("Enter End Port");
-            EndPort = Console.ReadLine();
-            int EPort = (int)Convert.ToInt64(EndPort);
-            if (SPort >= EPort)
-            {
-                Console.WriteLine("Error, Enter a number greater than Start Port");
-                Stager(IP);
-            }
-            if (EPort == 0)
-            {
-                Console.WriteLine("Error, Enter a number greater than 1");
-                Stager(IP);
-            }
-            //return (SPort, EPort, IP);
-            ThreadManager(SPort, EPort, IP);
-
-        }
-        static void ThreadManager(int SPort, int EPort, string IP)
-        {
-            test(0, 1);
-            Thread thread0 = new Thread(() => LoopSend0(SPort, EPort, IP));
-            Thread thread1 = new Thread(() => LoopSend1(SPort, EPort, IP));
-            Thread thread2 = new Thread(() => LoopSend2(SPort, EPort, IP));
-            Thread thread3 = new Thread(() => LoopSend3(SPort, EPort, IP));
-            Thread thread4 = new Thread(() => LoopSend4(SPort, EPort, IP));
-            thread0.Start();
-            thread0.IsBackground = true;
-            thread1.Start();
-            thread1.IsBackground = true;
-            thread2.Start();
-            thread2.IsBackground = true;
-            thread3.Start();
-            thread3.IsBackground = true;
-            thread4.Start();
-            thread4.IsBackground = true;
-
-        }  
-        static void LoopSend0(int portstart, int portend, string IP)
-        {
-            for (int testport = portstart; testport <= portend; testport = testport + 5)
-            {
-                SendPacket(IP, testport);
-            }
-        }
-        static void LoopSend1(int portstart, int portend, string IP)
-        {
-            for (int testport = portstart + 1; testport <= portend; testport = testport + 5)
-            {
-                SendPacket(IP, testport);
+                Console.WriteLine("Error: Enter a number greater than 0");
+                PortRange(IP); // Restart function if input is invalid
+                return;
             }
 
-        }
-        static void LoopSend2(int portstart, int portend, string IP)
-        {
-            Thread.Sleep(600);
-            for (int testport = portstart + 2; testport <= portend; testport = testport + 5)
+            Console.WriteLine("Enter End Port:");
+            int endPort = Convert.ToInt32(Console.ReadLine());
+
+            if (endPort <= startPort)
             {
-                SendPacket(IP, testport);
+                Console.WriteLine("Error: End Port must be greater than Start Port");
+                PortRange(IP); // Restart function if input is invalid
+                return;
+            }
+            ThreadManager(startPort, endPort, IP);
+        }
+        static void ThreadManager(int startPort, int endPort, string IP)
+        {
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                int threadStartPort = startPort + i;
+                threads[i] = new Thread(() => LoopSend(threadStartPort, endPort, IP));
+                threads[i].Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join(); // Wait for all threads to complete
+            }
+
+            openPorts.Sort();
+            Console.WriteLine("\nOpen Ports:");
+            foreach (var port in openPorts)
+            {
+                Console.WriteLine(port);
+            }
+
+            closedPorts.Sort();
+            Console.WriteLine("\nClosed Ports:");
+            foreach (var port in closedPorts)
+            {
+                Console.WriteLine(port);
             }
         }
-        static void LoopSend3(int portstart, int portend, string IP)
+        static void LoopSend(int startPort, int endPort, string IP)
         {
-            Thread.Sleep(900);
-            for (int testport = portstart + 3; testport <= portend; testport = testport + 5)
+            for (int testPort = startPort; testPort <= endPort; testPort += 10)
             {
-                SendPacket(IP, testport);
-            }
-        }
-        static void LoopSend4(int portstart, int portend, string IP)
-        {
-            Thread.Sleep(1200);
-            for (int testport = portstart + 4; testport <= portend; testport = testport + 5)
-            {
-                SendPacket(IP, testport);
+                SendPacket(IP, testPort);
             }
         }
         static void SendPacket(string IP, int port)
         {
-            using (TcpClient tcpClient = new TcpClient())
+            try
             {
-                int connection;
-                try
+                using (TcpClient tcpClient = new TcpClient())
                 {
-                    int oport;
+                    Console.WriteLine($"Sending Packets to {IP} On Port {port}");
                     tcpClient.Connect(IP, port);
-                    oport = port;
-                    Console.WriteLine($"Port {oport} open");
-                    connection = 2;
-                    test(oport, connection);
-                    //List(connection, port);
-
-                    //tcpClient.Close();
-
+                    AddToOpenPorts(port);
                 }
-                catch (Exception)
-                {
-                    int cport;
-                    cport = port;
-                    Console.WriteLine($"Port {cport} closed");
-                    connection = 3;
-                    test(cport, connection);
-                    //List(connection, port);
-                }             
             }
-        
+            catch (Exception)
+            {
+                AddToClosedPorts(port);
+            }
         }
-        static void test(int port, int connection)
+        static void AddToOpenPorts(int port)
         {
-            /*
-            int startarray;
-            if (connection == 1)
+            lock (openPorts)  
             {
-                startarray = 2;
-            }
-            else if (connection == 0)
-            {
-                startarray = 3;
-            }
-            else
-            {
-                startarray = 1;
-            }
-            */
-            switch (connection)
-            {
-                case 1:
-                    int[] openports;
-                    int[] closedports;
-                    // code block                    
-                    break;
-                case 2:
-                    openports = new int[] {port};
-                    // code                 
-                    break;
-                case 3:
-                    closedports = new int[] { port };
-                    // code block
-                    break;
-                default:
-                    Console.WriteLine("Error");
-                    // code block
-                    break;
+                openPorts.Add(port);
             }
         }
+        static void AddToClosedPorts(int port)
+        {
+            lock (closedPorts)
+            {
+                closedPorts.Add(port);
 
+            }
+        }
     }
 }
